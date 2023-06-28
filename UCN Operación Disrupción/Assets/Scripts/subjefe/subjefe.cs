316fad02baba0ec56204d6d518b7cc62577a2f4b
JFF00 <juan.maltes@alumnos.ucn.cl>
2023-06-27 20:16:03 -0400

    subjefe 1 avance

---
+++ UCN Operación Disrupción/Assets/Scripts/subjefe/subjefe.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class subjefe : Fighter
+	{
+    enum States { patrol, pursuit }
+
+    [SerializeField]
+    States state = States.patrol;
+    [SerializeField]
+    float searchRange = 10;
+    [SerializeField]
+    float stoppingDistance = 3;
+    [SerializeField]
+    float AttackCoolDown = 2;
+
+    Transform player;
+    Vector3 target;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        InvokeRepeating("SetTarget", 0, 5);
+        InvokeRepeating("SendAttack", 0, AttackCoolDown); 
+    }
+
+    void SendAttack()
+    {
+        if(state != States.pursuit)
+        {
+            return;
+        }
+        if(vel.magnitude != 0)
+        {
+            return;
+        }
+        if (!anim.GetCurrentAnimatorStateInfo(0).IsName("golpe") && !anim.GetCurrentAnimatorStateInfo(0).IsName("muerte"))
+        {
+            anim.SetTrigger("isGolpe");
+        }
+    }
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, searchRange);
+        Gizmos.DrawWireSphere(target, 0.2f);
+    }
+
+    void SetTarget()
+    {
+        if (state != States.patrol)
+            return;
+        target = new Vector2(transform.position.x + Random.Range(-searchRange, searchRange), Random.Range(LimitsY.y, LimitsY.x));
+    }
+
+    Vector2 vel;
+    void Update()
+    {
+        if (state == States.pursuit)
+        {
+            target = player.transform.position;
+            if (Vector3.Distance(target, transform.position) > searchRange * 1.2f)
+            {
+                target = transform.position;
+                state = States.patrol;
+                return;
+            }
+        }
+        else if (state == States.patrol)
+        {
+            var ob = Physics2D.CircleCast(transform.position, searchRange, Vector2.up);
+            if (ob.collider != null)
+            {
+                if (ob.collider.CompareTag("Player"))
+                {
+                    state = States.pursuit;
+                    return;
+                }
+            }
+        }
+        vel = target - transform.position;
+        sr.flipX = vel.x < 0;
+        if(vel.magnitude < stoppingDistance)
+            vel = Vector2.zero; 
+        vel.Normalize();
+        if (!anim.GetCurrentAnimatorStateInfo(0).IsName("golpe") && !anim.GetCurrentAnimatorStateInfo(0).IsName("muerte"))
+        {
+            anim.SetBool("isWalking", vel.magnitude != 0);
+        }
+        else
+        {
+            vel = Vector2.zero;
+        }
+       rb.velocity = new Vector2(vel.x * horizontalSpeed, vel.y * verticalSpeed);
+
+    }
+
+}
